ecb057c2f477a554fb6725ae0c201916f11c0939
cyberxdevsofficial <cyberxdevsofficial@gmail.com>
2025-09-24 19:15:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-function toggleMenu() {
-  document.querySelector('.nav-links').classList.toggle('active');
-}
+// Mobile Menu Toggle
+const menuToggle = document.getElementById('menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
